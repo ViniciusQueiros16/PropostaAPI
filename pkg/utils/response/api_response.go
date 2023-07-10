ddf27b798a99c7afc13cf0d17b37cb8c35575b57
Vinicius <vinny.queiyros16@gmail.com>
2023-07-10 18:50:25 -0300

    fix: fixed error cors

--- pkg/utils/response/api_response.go
+++ pkg/utils/response/api_response.go
@@ -7,8 +7,15 @@ import (
 )
 
 func ApiResponse(status int, body interface{}) (events.APIGatewayProxyResponse, error) {
-	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
-	resp.StatusCode = status
+	resp := events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Headers": "*",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "*",
+		},
+		StatusCode: status,
+	}
 
 	stringBody, _ := json.Marshal(body)
 	resp.Body = string(stringBody)
